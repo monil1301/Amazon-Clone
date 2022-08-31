819832bace105caa31feb15ed970124fff1bbfaa
monil1301 <70565653+monil1301@users.noreply.github.com>
2022-08-31 17:45:29 +0530

    Added AuthScreen

--- app/src/main/java/com/shah/amazonclone/ui/activity/AuthActivity.kt
+++ app/src/main/java/com/shah/amazonclone/ui/activity/AuthActivity.kt
@@ -4,16 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.shah.amazonclone.models.auth.SignUpDetails
-import com.shah.amazonclone.ui.component.auth.SignUpView
 import com.shah.amazonclone.ui.screen.AuthScreen
 import com.shah.amazonclone.ui.theme.AmazonCloneTheme
 
--- app/src/main/java/com/shah/amazonclone/ui/theme/Theme.kt
+++ app/src/main/java/com/shah/amazonclone/ui/theme/Theme.kt
@@ -2,13 +2,8 @@ package com.shah.amazonclone.ui.theme
 
 import android.app.Activity
 import android.os.Build
-import android.view.Window
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
@@ -16,7 +11,6 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat
 
 /**
  * Created by Monil Shah on 25/06/22.
